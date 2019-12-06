472679345630fd4202a49400a885fe6f503669f4
Kyle Chu <ktnc.985@gmail.com>
2019-12-06 19:31:41 +0800

    create pedestrian prefab and spawner script

---
+++ Assets/Scripts/PedestrianController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianController : MonoBehaviour
+{
+    //public GameObject destination;
+
+    public Transform target;
+    public float smoothTime = 3f;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        if (Vector3.Distance(targetPosition, transform.position) <= 1.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PedestrianSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianSpawner : MonoBehaviour
+{
+    public Transform target;
+    public float interval;
+    private float next_spawn_time;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        next_spawn_time = Time.time + interval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > next_spawn_time)
+        {
+            GameObject new_pedestrian = Instantiate(prefab, transform.parent);
+            new_pedestrian.GetComponent<PedestrianController>().target = target;
+            new_pedestrian.transform.parent = gameObject.transform.parent;
+            new_pedestrian.transform.localScale = new Vector3(1f, 1f, 1f);
+            new_pedestrian.transform.position = gameObject.transform.position;
+            next_spawn_time += interval;
+        }
+    }
+}
