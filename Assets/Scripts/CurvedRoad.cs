60a110612fd4e3b29236fecc453172c5969c6b84
Teclu <saintpatricktay@gmail.com>
2019-12-06 22:13:14 +0800

    Add Curved Road Toggle

--- Assets/Scripts/CurvedRoad.cs
+++ Assets/Scripts/CurvedRoad.cs
@@ -6,9 +6,22 @@ public class CurvedRoad : Road
 {
     public Road RoadA, RoadB1, RoadB2;
     public bool IsToggable, ToggleRotate;
+    public GameObject ToggleSprite;
+    public GameObject RoadSprite;
     
     private void Start()
     {
         Type = RoadType.Curved;
+        ToggleSprite.SetActive(IsToggable);
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("YO");
+        if (IsToggable)
+        {
+            ToggleRotate = !ToggleRotate;
+            RoadSprite.transform.Rotate(Vector3.forward, (ToggleRotate) ? -90.0f : 90.0f);
+        }
     }
 }
