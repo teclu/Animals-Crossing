d7b75304441c5a66c58880d36562a4e36dac82b5
Teclu <saintpatricktay@gmail.com>
2019-12-06 18:46:28 +0800

    Basic Train and Track Implementation

--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Train : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Track PreviousTrack, CurrentTrack;
+
+    private float speed = 5.0f;
+
+    private void Start()
     {
-        
+        if (PreviousTrack == null && CurrentTrack != null)
+        {
+            PreviousTrack = CurrentTrack;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (CurrentTrack == null)
+        {
+            Image image = GetComponent<Image>();
+            image.color = new Color(image.color.r, image.color.g, image.color.b, image.color.a - 1.0f * Time.deltaTime);
+            if (image.color.a == 0.0f)
+            {
+                GameObject.Destroy(this.gameObject);
+            }
+            return;
+        }
+
+        float step =  speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, CurrentTrack.Node.position, step);
+        if (Vector3.Distance(transform.position, CurrentTrack.Node.position) < 0.001f)
+        {
+            Track tempTrack = CurrentTrack;
+            CurrentTrack = (PreviousTrack == CurrentTrack.TrackA) ? CurrentTrack.TrackB : CurrentTrack.TrackA;
+            PreviousTrack = tempTrack;
+        }
     }
 }
