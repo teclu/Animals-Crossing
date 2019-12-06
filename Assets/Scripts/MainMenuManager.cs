233145cfb405f31a6ae244027ecc01c967bc6633
Teclu <saintpatricktay@gmail.com>
2019-12-07 02:03:07 +0800

    Skeleton Main Menu and Scene Management

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject TitleScreen, CreditsScreen;
+
+    private void Start()
+    {
+        TitleScreen.SetActive(true);
+        CreditsScreen.SetActive(false);
+    }
+    
+    public void OnClickStartGame()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void OnClickCredits()
+    {
+        TitleScreen.SetActive(false);
+        CreditsScreen.SetActive(true);
+    }
+
+    public void OnClickBackToTitleScreen()
+    {
+        TitleScreen.SetActive(true);
+        CreditsScreen.SetActive(false);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
