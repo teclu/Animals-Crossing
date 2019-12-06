6cf3516ff09840465e8877323b99b11e3889563d
Teclu <saintpatricktay@gmail.com>
2019-12-07 04:08:47 +0800

    Round Countdown Timer

--- Assets/Scripts/Managers/GameLevelManager.cs
+++ Assets/Scripts/Managers/GameLevelManager.cs
@@ -7,12 +7,15 @@ using TMPro;
 public class GameLevelManager : MonoBehaviour
 {
     public TextMeshProUGUI DeathCount;
+    public TextMeshProUGUI TimerCountDown;
 
+    public float TimerCountDownValue;
     private int deathCount = 0;
 
     private void Start()
     {
         DeathCount.text = "Deaths: 0";
+        TimerCountDown.text = "Time Left: " + (int) TimerCountDownValue;
     }
 
     private void OnEnable()
@@ -29,5 +32,24 @@ public class GameLevelManager : MonoBehaviour
     {
         deathCount++;
         DeathCount.text = "Deaths: " + deathCount;
+    }
+
+    private void Update()
+    {
+        if (PauseMenuManager.isGamePaused)
+        {
+            return;
+        }
+
+        if (TimerCountDownValue > 0)
+        {
+            TimerCountDownValue -= Time.deltaTime;
+            
+            if (TimerCountDownValue <= 0.0f)
+            {
+                TimerCountDownValue = 0.0f;
+            }
+        }
+        TimerCountDown.text = "Time Left: " + (int) TimerCountDownValue;
     }
 }
